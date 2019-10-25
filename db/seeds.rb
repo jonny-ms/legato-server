68ca9e1448826b2583385617f864510c12cd70fc
Jonathan Milne-Skarzenski <j.milne21@gmail.com>
2019-10-25 19:36:26 +0000

    Initial test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,16 @@
 teacher1 = Teacher.create!({
   first_name: "Aaron",
   last_name: "Tagadtad",
-  email: "a.tag@gmail.com"
+  email: "a.tag@gmail.com",
+  password: "password",
+  profile_pic: "https://www.arisecity.ca/uploads/6/9/2/8/6928203/published/aaron.jpeg?1554954339",
+  bio: "Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before. Many say exploration is part of our destiny, but it’s actually our duty to future generations and their quest to ensure the survival of the human species.",
+  address: {"street": "662 King St",
+            "city": "Toronto",
+            "country": "Canada"
+          },
+  postal_code: "M5V 1M7",
+  certifications: nil,
+  will_host: true,
+  will_travel: false
 })
