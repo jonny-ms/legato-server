162fdfc800636f46dc00ed55489f369bdadc4ebe
Aaron Tagadtad <aarontagadtad@Aaron-Tagadtads-MacBook-Pro.local>
2019-10-28 16:44:37 -0400

    added index in course controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'courses/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/api' do
     root to: 'teachers#index'
   
     resources :teachers
+    resources :courses
     # resources :students, only: [:index]
 
     resources :sessions, only: [:create]
