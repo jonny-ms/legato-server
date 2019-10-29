c2943f1e715a3b5e0c1683632ad822238e38472d
CameronDunning <dunningcameron@gmail.com>
2019-10-29 17:13:30 -0400

    Fix booking bug

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,5 +2,5 @@ class Timeslot < ApplicationRecord
   belongs_to :teacher
   belongs_to :lesson, optional: true
 
-  validates :datetime, presence: true, uniqueness: true
+  validates :datetime, presence: true, uniqueness: { scope: :teacher_id }
 end
