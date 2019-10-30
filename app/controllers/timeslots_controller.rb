30f4d042e90178a98cbf810c58d5ae0828be32c7
CameronDunning <dunningcameron@gmail.com>
2019-10-30 18:22:14 -0400

    Add teacher accept booking request

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -11,10 +11,19 @@ class TimeslotsController < ApplicationController
 
   def create
     # Add a teacher's availability to the database
-    Timeslot.where(teacher_id: session[:teacher_id]).destroy_all
+    # Timeslot.where(teacher_id: session[:teacher_id]).destroy_all
     timeslot_params.each do |x|
       Timeslot.create(datetime: x, teacher_id: session[:teacher_id], is_booked: false)
     end
+  end
+
+  def update
+
+    p timeslot_params
+
+    @timeslot = Timeslot.find(timeslot_params)
+    @timeslot.update(is_booked: true)
+    
   end
 
   def destroy
