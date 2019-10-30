d350dd7f77d849be7c53788b373d0df8d1d1f40f
Jonathan Milne-Skarzenski <j.milne21@gmail.com>
2019-10-30 01:41:58 +0000

    Working on booking requests

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,15 @@
 class LessonsController < ApplicationController
 
   def create
+    p lesson_params
+
+    lesson_params.each do |x|
+      @lesson = Lesson.new(student_id: session[:student_id], course_id: '1')
+      @timeslot = Timeslot.find(x)
+      @lesson.save
+      @timeslot.update(lesson_id: @lesson.id)
+    end
+    
   end
 
 
