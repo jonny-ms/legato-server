be64819a74f581bbc443d0a6f0cc38355160fe15
Jonathan Milne-Skarzenski <j.milne21@gmail.com>
2019-10-30 15:24:54 +0000

    Add course_id to lesson post

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,10 +3,9 @@ class LessonsController < ApplicationController
   def create
     # Create a lesson once a student has requested an appointment - not a booking until the teacher confirms
     # TODO: Set 1 lesson for multiple timeslots if the timeslots are in a row
-    p lesson_params
 
-    lesson_params.each do |x|
-      @lesson = Lesson.new(student_id: session[:student_id], course_id: '1')
+    lesson_params[:timeslots].each do |x|
+      @lesson = Lesson.new(student_id: session[:student_id], course_id: lesson_params[:course_id])
       @timeslot = Timeslot.find(x)
       @lesson.save
       @timeslot.update(lesson_id: @lesson.id)
@@ -18,7 +17,7 @@ class LessonsController < ApplicationController
     
   private
     def lesson_params
-      params.require(:lesson)
+      params.require(:lesson).permit(:course_id, timeslots: [])
     end
   
 end
